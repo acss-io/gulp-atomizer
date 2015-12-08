509cf4f66372b1b1d7f252da918b3bf913334d83
Caleb Whiting <whmountains@icloud.com>
2015-12-08 20:35:53 -0300

    mostly done
    but still fighting with error

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-var through = require('through2')
-// var Atomizer = require('atomizer')
+var through  = require('through2')
+var Atomizer = require('atomizer')
+var _        = require('lodash')
+var File     = require('vinyl')
 
 // Parse text to find Atomic CSS classes
 // var foundClasses = atomizer.findClassNames();
@@ -10,16 +12,86 @@ var through = require('through2')
 // Generate Atomic CSS from configuration
 // var css = atomizer.getCss(finalConfig);
 
-module.exports = function() {
+module.exports = function(config) {
 
-  // var defaultConfig = {}
-  // var foundClasses = []
-  // var atomizer = new Atomizer()
+  // default the config to an empty object
+  var acssConfig = config || {}
 
-  return through.obj(function(file, encoding, cb) {
-    console.log('file: ' + file)
-    console.log('encoding: ' + encoding)
-    cb(null, file)
-  })
+  //global variables
+  var latestFile
+  var latestMod
+  var foundClasses = []
+  var acss
+
+  // create the file handler
+  var gulpTransformer = function(file, unused, cb) {
+
+    if (file.isNull()) {
+        // nothing to do
+        return cb(null, file);
+    }
+
+    if ( ! File.isVinyl(file)) {
+      // What!!!!  We're not being given a vinyl file!?!?!?!
+      return cb(null, file)
+    }
+
+    else if (file.isStream()) {
+      // file.contents is a Stream.  We don't support streams
+      this.emit('error', new PluginError(PLUGIN_NAME, 'Streams not supported!'));
+    }
+
+    else if (file.isBuffer()) {
+
+      //lazy init the acss class
+      if (!acss) {
+        acss = new Atomizer({verbose: true})
+      }
+
+      // generate the class names and push them into the global collector array
+      var html = String(file.contents)
+      var classes = acss.findClassNames(html)
+      foundClasses.push(classes)
+
+      // make a note of this file if it's the newer than we've seen before
+      if (!latestMod || file.stat && file.stat.mtime > latestMod) {
+        latestFile = file
+        latestMod = file.stat && file.stat.mtime
+      }
+
+      // tell the engine we're done
+      cb()
+
+    }
+
+  }
+
+  var endStream = function(cb) {
+
+    // nothing in, nothing out
+    if (!latestFile || !acss) {
+      cb();
+      return;
+    }
+
+    // remove duplicate classes
+    var classes = _.uniq(foundClasses)
+    // merge the classes into the user's config
+    var finalConfig = acss.getConfig(foundClasses, acssConfig);
+    // get the actual css
+    var cssOut = acss.getCss(finalConfig)
+
+    // create the output file
+    // (take the metadata from most recent file)
+    var atomicFile = latestFile.clone({contents: false});
+    atomicFile.contents = new Buffer(cssOut)
+
+    // all done!
+    this.push(atomicFile)
+    cb()
+
+  }
+
+  return through.obj(gulpTransformer, endStream)
 
 }
---
+++ test/test.js
@@ -0,0 +1,40 @@
+/*eslint-env node, mocha */
+
+var atomizer = require('../')
+var vfs      = require('vinyl-fs')
+var fs       = require('fs')
+var es       = require('event-stream')
+var assert   = require('assert')
+
+describe('gulp-atomizer', function() {
+  describe('in buffer mode', function () {
+    it('should find acss classnames in html files', function (td) {
+
+      //init our transformer
+      var acss = atomizer({
+        "custom": {
+          "Bgc($primary)": "#f3f3f3"
+        }
+      })
+
+      //pipe the two test files to acss
+      vfs.src(['./test1.html', './test2.html'], {cwd: __dirname})
+        .pipe(acss)
+
+      acss.once('data', function(file) {
+        // make sure it came out the same way it went in
+        assert(file.isBuffer());
+
+        // read the template file
+        var template = fs.readFileSync(__dirname + '/atomic.css', 'utf8')
+
+        // stringify the buffer
+        var contents = file.contents.toString('utf8')
+
+        assert.equal(contents, template, 'acss output isnt correct')
+
+        td()
+      })
+    })
+  })
+})
